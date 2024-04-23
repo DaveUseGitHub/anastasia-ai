6806320e9fb932b09c26fc2d71dca4e584477d00
DaveUseGitHub <83485367+DaveUseGitHub@users.noreply.github.com>
2024-04-23 22:09:24 +0700

    Update main.py
    Adding login, logout and new chat widget

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import boto3
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+from streamlit_authenticator.utilities.exceptions import (CredentialsError,
+                                                          ForgotError,
+                                                          LoginError,
+                                                          RegisterError,
+                                                          ResetError,
+                                                          UpdateError)
+
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain_community.chat_models import BedrockChat
 from langchain_community.embeddings import BedrockEmbeddings
@@ -9,9 +19,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_community.vectorstores import FAISS
 from langchain_core.messages import HumanMessage, AIMessage
-
-st.set_page_config(page_title='Start Consulting', layout='wide', page_icon="./fav.ico")
-st.title("Chat with Anastasia")
 
 bedrock_runtime = boto3.client(
     service_name="bedrock-runtime",
@@ -39,7 +46,7 @@ def vector_store(docs):
 def claude_model():
     llm = BedrockChat(
         client=bedrock_runtime,
-        model_id="anthropic.claude-3-haiku-20240307-v1:0",
+        model_id="anthropic.claude-3-sonnet-20240229-v1:0",
         model_kwargs={
             "max_tokens": 2048,
             "temperature": 0.9,
@@ -49,7 +56,11 @@ def claude_model():
 
 prompt_template = """
 You're an AI expert in psychology and psychiatry named Anastasia.
-You have several task is to give mental health and personal development advice to users and become their mental support. You will be confused if the user ask about something out of topic. Only give the user about the professional data when only they ask. Be smart and creative. don't throw the same answer in every response. 
+You have several task are to give mental health and personal development advice to users and become their mental support. 
+You will be confused if the user ask about something out of topic. 
+Only give the user about the professional data when only they ask. 
+Be smart and creative. don't throw the same answer in every response.
+Your response's language based on user language.  
 
 <context>
 {context} 
@@ -60,6 +71,7 @@ Assistant:"""
 
 PROMPT = PromptTemplate(template=prompt_template,
                         input_variables=["context", "question"])
+
 
 def get_response_llm(llm, vectorstore_faiss, query):
     qa = RetrievalQA.from_chain_type(
@@ -77,40 +89,68 @@ def get_response_llm(llm, vectorstore_faiss, query):
 if "memory" not in st.session_state:
     st.session_state.memory = StreamlitChatMessageHistory()
 
-# Sidebar info
-with st.sidebar:
-    st.image('./fav.ico', width=100, use_column_width= "never")
-    st.write("## Anastasia.ai")
-    st.caption("Personal AI Assistant in Mental Health and Personal Development")
-
-    st.divider()
-    st.markdown("If you want to ask about our mental health professional's information. Make sure to click 'Sync database'first")
-    if st.button("Sync database"):
-        with st.spinner("Processing..."):
-            docs = data_ingestion()
-            vector_store(docs)
-            st.success("Done")
+def reset_chat() -> None:
+    st.session_state.memory = StreamlitChatMessageHistory()
+    st.session_state.memory.messages = []
     
-# Conversation
-for message in st.session_state.memory.messages:
-    if isinstance(message, HumanMessage):
-        with st.chat_message("Human"):
-            st.markdown(message.content)
-    else:
-        with st.chat_message("AI"):
-            st.markdown(message.content)
+st.set_page_config(page_title='Start Consulting', layout='wide', page_icon="./fav.ico")
 
-# User input
-user_input = st.chat_input()
-if user_input:
-    human_message = HumanMessage(content=user_input)
-    st.session_state.memory.add_user_message(human_message)
-    with st.chat_message("Human"):
-        st.markdown(user_input)
-    with st.chat_message("AI"):
-        faiss_index = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-        llm = claude_model()
-        response = get_response_llm(llm, faiss_index, user_input)
-        ai_message = AIMessage(content=response)
-        st.write(response)
-        st.session_state.memory.add_ai_message(ai_message)
+with open('config.yaml', 'r', encoding='utf-8') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Creating the authenticator object
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+try:
+    authenticator.login()
+except LoginError as e:
+    st.error(e)
+
+if st.session_state["authentication_status"]:
+    st.button("New Chat", on_click=reset_chat, type='primary')
+    st.title("Consult with Anastasia")
+    st.write("   Hi! I'm Anastasia. How may I help you?")
+    with st.sidebar:
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        authenticator.logout()
+        st.divider()
+        st.image('./fav.ico', width=100, use_column_width= "never")
+        st.write("## Anastasia.ai")
+        st.caption("Personal AI Assistant in Mental Health and Personal Development")
+        st.caption("Several use cases: ")
+        st.caption("1. Consultation")
+        st.caption("2. Discussion")
+        st.caption("3. Ask for medical check-up with real mental health professional")
+    for message in st.session_state.memory.messages:
+        if isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.markdown(message.content)
+        else:
+            with st.chat_message("AI"):
+                st.markdown(message.content)
+    
+    # User input
+    user_input = st.chat_input()
+    if user_input:
+        human_message = HumanMessage(content=user_input)
+        st.session_state.memory.add_user_message(human_message)
+        with st.chat_message("Human"):
+            st.markdown(user_input)
+        with st.chat_message("AI"):
+            faiss_index = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+            llm = claude_model()
+            response = get_response_llm(llm, faiss_index, user_input)
+            ai_message = AIMessage(content=response)
+            st.write(response)
+            st.session_state.memory.add_ai_message(ai_message)
+
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
